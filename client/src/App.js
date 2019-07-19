0af1488b8478f6ad4c967c3ab95c852b66ce8713
carlos <ccarlosa@uninorte.edu.co>
2019-07-19 12:10:20 -0500

    React Started

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Navbar from './components/layouts/Navbar';
+const App = () => (
+  <Fragment>
+    <Navbar />
+  </Fragment>
+);
+
+export default App;
--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middlewares/auth');
+const db = require('../../config/db');
 
-router.get('/', (req, res) => {
+// @route   GET api/orders
+// @desc    Get all orders
+// @access  Private
+router.get('/', auth, (req, res) => {
   res.send('router working - order section');
 });
 
+// @route   GET api/orders
+// @desc    Get all orders
+// @access  Private
+router.post('/register', auth, async (req, res) => {
+  const { remision, cedula, pickup, deliver, driver } = req.body;
+  let time = new Date(Date.now());
+  time = time.toString().split('GMT')[0];
+
+  let newOrder = {
+    remision,
+    cedula,
+    pickup,
+    deliver,
+    time,
+    user: req.user.name,
+    driver
+  };
+  await db.query('INSERT INTO Orders set ?', [newOrder]);
+  res.json('New order added');
+});
 module.exports = router;
